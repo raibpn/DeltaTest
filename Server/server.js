b977d731945bc901d810eb932ccea51a4bba5844
Bipin Rai <bipi0007@edu.easj.dk>
2021-06-15 21:48:12 +0200

    get users

--- Server/server.js
+++ Server/server.js
@@ -14,7 +14,7 @@ app.use('/api/users', userRoutes )
 //   res.json({ message: "API running..." });
 // });
 
-// app.use("/api/products", productRoutes);
+app.use("/admin", adminRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
