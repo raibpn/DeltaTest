0e88e25fc71ca29b1ce61f595170da826320a6c4
Bipin Rai <bipi0007@edu.easj.dk>
2021-06-16 10:59:47 +0200

    JWT

--- Server/server.js
+++ Server/server.js
@@ -1,24 +1,20 @@
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
-require("dotenv").config();
+require("dotenv").config(); //won't be pushing local/db credentials to git
 const express = require("express");
 const userRoutes = require("./routes/userRoutes");
 const connectDB = require("./config/db");
-
 connectDB();
+
+const auth = require('./routes/auth');
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/users', userRoutes )
-
-// app.get("/", (req, res) => {
-//   res.json({ message: "API running..." });
-// });
-
-
 app.use("/admin", adminRoutes);
+app.use('/api/auth', auth);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
